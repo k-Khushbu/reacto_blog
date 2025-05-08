9970f11fa33245fd04f730e9ec74f6957dd58400
Khushbu K. <khushukat@gmail.com>
2025-05-08 19:27:33 +0530

    commit project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>Reacto Blog website</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
